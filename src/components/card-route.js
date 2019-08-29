621c20d29a29e38e3beed51bbe922671a9ea0c1c
germiona <violetta.ashirova@gmail.com>
2019-08-29 21:00:17 +0300

    linters

--- src/components/card-route.js
+++ src/components/card-route.js
@@ -1,6 +1,6 @@
 export const createCardRoute = ({cityStart, cityTransfer, cityEnd, startMonth, startDate, endDate}) => `
   <div class="trip-info__main">
-    <h1 class="trip-info__title">${cityStart} &mdash; ${cityTransfer !== ``? `...&mdash;` : ``}   ${cityEnd}</h1>
+    <h1 class="trip-info__title">${cityStart} &mdash; ${cityTransfer !== `` ? `...&mdash;` : ``}   ${cityEnd}</h1>
     <p class="trip-info__dates">${startMonth} ${startDate.getUTCDate()}&nbsp;—&nbsp;${endDate.getUTCDate()}</p>
   </div>
   `;
